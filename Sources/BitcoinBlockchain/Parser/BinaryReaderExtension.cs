c4a4c5f80d6947b2d56405a45db99144018d5c60
Ladi Molnar <ladimolnar02@outlook.com>
2015-05-04 23:28:18 -0700

    Optimize the case where a blockchain file ends in a large number of zero bytes.

--- Sources/BitcoinBlockchain/Parser/BinaryReaderExtension.cs
+++ Sources/BitcoinBlockchain/Parser/BinaryReaderExtension.cs
@@ -26,18 +26,60 @@ namespace BitcoinBlockchain.Parser
         /// </returns>
         internal static bool SkipZeroBytes(this BinaryReader binaryReader)
         {
+            const int bufferMaxSize = 1000000;
+
             int peekCharResult;
-            do
+            if (binaryReader.BaseStream.CanSeek)
             {
-                peekCharResult = binaryReader.PeekChar();
-                if (peekCharResult == 0)
+                int skipedBytes = 0;
+                while ((peekCharResult = binaryReader.PeekChar()) == 0 && skipedBytes < 1000)
+                {
+                    binaryReader.ReadBytes(1);
+                    skipedBytes++;
+                }
+
+                if (peekCharResult != 0)
+                {
+                    return peekCharResult > 0;
+                }
+
+                // We already read 1000 zero bytes. Chances are that the remaining file contains only 0. 
+                // We'll start reading larger blocks and seek to the correct position as needed.
+                bool bufferIsZeroed;
+                bool endOfFileReached;
+
+                do
+                {
+                    long streamPosition = binaryReader.BaseStream.Position;
+
+                    byte[] buffer = binaryReader.ReadBytes(bufferMaxSize);
+
+                    endOfFileReached = buffer.Length < bufferMaxSize;
+
+                    bufferIsZeroed = true;
+                    for (int i = 0; i < buffer.Length; i++)
+                    {
+                        if (buffer[i] != 0)
+                        {
+                            bufferIsZeroed = false;
+                            binaryReader.BaseStream.Seek(streamPosition + i, SeekOrigin.Begin);
+                            break;
+                        }
+                    }
+                }
+                while (bufferIsZeroed && endOfFileReached == false);
+
+                return (endOfFileReached && bufferIsZeroed) == false;
+            }
+            else
+            {
+                while ((peekCharResult = binaryReader.PeekChar()) == 0)
                 {
                     binaryReader.ReadBytes(1);
                 }
-            }
-            while (peekCharResult == 0);
 
-            return peekCharResult > 0;
+                return peekCharResult > 0;
+            }
         }
     }
 }
