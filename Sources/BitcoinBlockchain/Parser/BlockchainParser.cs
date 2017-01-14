aba7064cc2bc943f2439443b827ebe90194bfcf6
Ladi Molnar <ladimolnar02@outlook.com>
2017-01-13 19:33:19 -0800

    Accommodate for block version 0x20000002. Increase the assembly version, update the .nuspec file.

--- Sources/BitcoinBlockchain/Parser/BlockchainParser.cs
+++ Sources/BitcoinBlockchain/Parser/BlockchainParser.cs
@@ -161,9 +161,10 @@ namespace BitcoinBlockchain.Parser
                 blockHeader.BlockVersion != 0x20000000 &&
                 blockHeader.BlockVersion != 0x20000001 && 
                 blockHeader.BlockVersion != 0x30000001 &&
-                blockHeader.BlockVersion != 0x08000004)
+                blockHeader.BlockVersion != 0x08000004 &&
+                blockHeader.BlockVersion != 0x20000002)
             {
-                throw new UnknownBlockVersionException(string.Format(CultureInfo.InvariantCulture, "Unknown block version: {0}.", blockHeader.BlockVersion));
+                throw new UnknownBlockVersionException(string.Format(CultureInfo.InvariantCulture, "Unknown block version: {0} ({0:X}).", blockHeader.BlockVersion));
             }
 
             blockHeader.PreviousBlockHash = new ByteArray(blockMemoryStreamReader.ReadBytes(32).ReverseByteArray());
